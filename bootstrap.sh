7e9adc0cd462c5df9babf6bfee71c00b69ff6e2e
hrnrhty <hrnrhty@gmail.com>
2013-12-18 01:10:46 +0900

    Update version

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@
 #==== Script settings ====                                                 {{{
 
 if [ -z "$VERSION"] ; then
-    VERSION="0.0.2"
+    VERSION="0.0.3"
 fi
 
 if [ -z "$DOTFILES"] ; then
