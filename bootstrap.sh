38e687758ef8d6c1ee70b85cf1952a61f8945302
hrnrhty <hrnrhty@gmail.com>
2014-01-24 11:42:54 +0900

    Change version to 0.0.5

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@
 #==== Script settings ====                                                 {{{
 
 if [ -z "$VERSION"] ; then
-    VERSION="0.0.4"
+    VERSION="0.0.5"
 fi
 
 if [ -z "$DOTFILES"] ; then
