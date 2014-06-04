a94df9ab6b837bebcb51dba92baab317894453c5
hrnrhty <hrnrhty@gmail.com>
2014-06-04 13:22:35 +0900

    [bootstrap.sh] update version; 0.1.11

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@
 #==== Script settings ====                                                 {{{
 
 if [ -z "$VERSION"] ; then
-    VERSION="0.1.10"
+    VERSION="0.1.11"
 fi
 
 if [ -z "$DOTFILES"] ; then
